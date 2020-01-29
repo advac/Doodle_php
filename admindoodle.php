f4cc0dc9da7ad1e6392011add8494ef6b9a790ec
Advac <48288487+advac@users.noreply.github.com>
2020-01-29 10:14:52 +0100

    ajout SELECT

--- admindoodle.php
+++ admindoodle.php
@@ -1,45 +1,70 @@
-<?php
+<?php 
     include("includes/bdd.inc.php");
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gestionnaire de rendez-vous pour l'admin</title>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>Gestionnaire de rendez-vous pour l'administrateur</title>
+		
+	</head>
 <body>
-    <h1>Gestionnaire de rendez-vous pour l'admin</h1>
-    <h3>Dates et heures proposées</h3>
-    <form method="GET" action="recorddatetime.php">
-        <p>
-            Numéro d'évenement:
-            <input type="text" name="event">
-            <input type="date" name="date">
-            <input type="time" name="heure">
-        </p>
-        <input type="submit" value="Valider le formulaire">
-        <div>
-            <?php
-                $sql = "SELECT * FROM doodle WHERE numero_evenement = 1";
-                $resultat = mysqli_query($lien, $sql);
-                if(!$resultat)
-                {
-                    echo "ERROR : BDD";
-                }
-                while($ligne = mysqli_fetch_array($resultat))
-                {
-                    $dateheure = $ligne['dateheure'];
-                    $nombre_participants = $ligne['nombre_participants'];
-                    echo "<p>$dateheure $nombre_participants</p>";
-                }
+<h1>Gestionnaire de rendez-vous pour l'administrateur</h1>
+<?php
+    $sql = "SELECT DISTINCT numero_evenement FROM doodle"; //select tout les elements du doodle : DISTINCT pour afficher sans doublons
+    $resultat = mysqli_query($lien, $sql);
+    if(!$resultat)
+    {
+        echo "ERROR : BDD";
+    }
+    echo "<select id='liste' onchange='onchangeselect();'>";    //on fabrique un select de l'evenement
+    echo "<option selected>Sélectionner un numéro d'évenement</option>"; //rajout d'un select demandant la selection à user
+    while($ligne = mysqli_fetch_array($resultat))
+    {
+        echo "<option>";
+        echo $ligne["numero_evenement"];
+        echo "</option>";
+    }
+    echo "</select>";
 
-            ?>
-        </div>
-    </form>
-    <script src="jquery.js" type="text/javascript"></script>  
-    <script language="javascript" type="text/javascript"></script>
+?>
+<h3>Dates et heures proposées</h3>
+<form method="GET" action="recorddatetime.php">
+    <p>
+        Numero d'évènement : 
+        <input type="text" id="event" name="event">
+        <input type="date" name="date">
+        <input type="time" name="time">
+    </p>
+    <input type="submit" value="enregistrer">
+    <div>
+        <?php 
+            $sql = "SELECT * FROM doodle WHERE numero_evenement = 1";
+            $resultat = mysqli_query($lien, $sql);
+            if(!$resultat){
+                echo "ERROR : BDD";
+            }
+            while($ligne = mysqli_fetch_array($resultat))
+            {
+                $dateheure =$ligne["dateheure"];
+                $nombre_participant =$ligne["nombre_participants"];
+                echo "<p>evenement avec $nombre_participant participant le : $dateheure</p>";
+            }
+        ?>
+    </div>
+</form>
+	<script src="js/jquery.js" type="text/javascript"></script>  
+	<script language="JavaScript" type="text/javascript">
+        function onchangeselect()
+        {
+            var index = document.getElementById("liste").selectedIndex;
+            var texte = document.getElementById("liste").options[index].text;
+            if(texte != "Sélectionner un numéro d'évenement")
+            {
+                document.getElementById("event").value = texte;
+            }
+        }
+	</script>
 </body>
 </html>
+
--- recorddatetime.php
+++ recorddatetime.php
@@ -1,23 +1,50 @@
 <?php
-header("Content-Type: text/plain ; charset=utf-8");
-header("Cache-Control: no-cache , private");
-header("Pragma: no-cache");
-include("includes/bdd.inc.php");
+    header("Content-Type: text/plain ; charset=utf-8");
+    header("Cache-Control: no-cache , private");
+    header("Pragma: no-cache");
+    include("includes/bdd.inc.php");
 
-if(isset($_REQUEST['event']) && isset($_REQUEST['date']) && isset($_REQUEST['time'])){
-    $event = intval($_REQUEST['event']);
-    $date = $_REQUEST['date'];
-    $time = $_REQUEST['time'];
-    $nombre_participant = 0;
-    $datetime = new DateTime($date . ' ' . $time);
-    $dateFormatee = date_format($datetime, 'Y-m-d H:m:s');
-    $sql = "INSERT INTO doodle (dateheure, nombre_participants, numero_evenement) VALUES ('$dateFormatee' , $nombre_participant , $event )";
-    if (mysqli_query($lien, $sql)) {
-        echo "New record created successfully";
-     } else {
-        echo "Error: " . $sql . "" . mysqli_error($lien);
-     }
-     $lien->close();
-     header("Location: http://localhost/doodle/admindoodle.php");
-}
+    $file = fopen("recordedatetimelog.txt", 'a');
+    function write2log($file, $texte)
+    {
+        $objDateTime = new DateTime('NOW');
+        $dateFormatee = date_format($objDateTime, 'd/m/y H:i:s:ms : ') . $texte . "\n";
+        fwrite($file, $dateFormatee);
+    }
+
+    if(isset($_REQUEST["event"])&& $_REQUEST['event'] != "")
+    {
+        $numero_evenement = $_REQUEST['event'];
+    }else{
+        echo "ERROR : input (évènement manquant)";
+        write2log($file, "ERROR : input (évènement manquant)");
+        exit();
+    }
+    if(isset($_REQUEST["date"])&& $_REQUEST['date'] != "")
+    {
+        $date = $_REQUEST['date'];
+    }else{
+        echo "ERROR : input (date manquant)";
+        write2log($file, "ERROR : input (date manquant)");
+        exit();
+    }
+    if(isset($_REQUEST["time"])&& $_REQUEST['time'] != "")
+    {
+        $time = $_REQUEST['time'];
+    }else{
+        echo "ERROR : input (time manquant)";
+        write2log($file, "ERROR : input (time manquant)");
+        exit();
+    }
+    $sql = "INSERT INTO doodle (id_doodle, dateheure, nombre_participants, numero_evenement) VALUES (NULL, '$date $time', 0, $numero_evenement)"; 
+    $resultat = mysqli_query($lien, $sql);
+    if(!$resultat)
+    {
+        echo "ERROR : database (INSERT)";
+        write2log($file, "ERROR : database (INSERT)");
+    }
+    header("location: http://localhost/AJAX/DOODLE/Doodle_php/admindoodle.php");
+    //echo "nouvel évènement numéro ", $numero_evenement, " le ",$date, " à ", $time;
+
+    fclose($file);
 ?>
